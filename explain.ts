b1013d1dbc2c5e8b59c513fac660118bd2d8492f
Michael-Hasan <abdukhodirovhasan2002@gmail.com>
2025-11-09 23:34:42 +0900

    feat: React Router

---
+++ explain.ts
@@ -0,0 +1,16 @@
+//  EXPLAINED ALL DETAILS  //
+
+// React - single page applicationlarning bir turi;
+// React - bu kutubxona(library), u Virtual dom va Real domni bog'lab beruvchi vosita(reconsulation process);
+// Real dom - bu hozirgacha ishlatgan documentimiz, ejs tog'ridan tog'ri Real dom bilan ishladik;
+// Virtual dom - bu real domning virtual korinishidir, yani yozuvli virtual korinishi;
+// Yarn - frontenda packagelar katta boladi, npm katta packagelarda xatolik sodir qilib qoyishi mumkin, shuning uchun Yarn ishlatiladi;
+// React - reactda HTML browserda quriladi, backendan faqat json fromatdagi data olinadi;
+// Real dom bolishi kerak, virtual dom ishlashi uchun, real dom esa index.html ni ichida "root";
+// Component - Reactda hamma narsa componentdan iborat, ular declarative xususiyatga ega, bitta component qurib, uni xohlagan joyda ishlatishimiz mumkin;
+// React - bu library, u bizga component-based, declarative single-page applicationni qurib beradi;
+// Declarative - qaysidur payti mana bu componentni ochasan, qolgan componentlarni yopasan deydi yani declare qiladi, condiotionga oxshaydi;
+
+// FronEndda router qurishda React Router-Dom degan packagedan foydalanamiz, react bu library, uning router tizimi mavjud emas;
+// Screen - componenti bu butun bir oyna, pageimizni ifodalab keladigan componentdir, uni folderlar orqali hosil qilamiz;
+// Componentlar 3 xili mavjuda: screen, sectional & reusable components;
