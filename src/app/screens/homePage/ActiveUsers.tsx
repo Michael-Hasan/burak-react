d346e952ca1f6b6bcc469386bee9bd2ff6a84b64
Michael-Hasan <abdukhodirovhasan2002@gmail.com>
2025-12-08 00:54:46 +0900

    feat: Backend develop yakun

--- src/app/screens/homePage/ActiveUsers.tsx
+++ src/app/screens/homePage/ActiveUsers.tsx
@@ -3,15 +3,19 @@ import Card from "@mui/joy/Card";
 import { CssVarsProvider, Typography } from "@mui/joy";
 import CardOverflow from "@mui/joy/CardOverflow";
 import AspectRatio from "@mui/joy/AspectRatio";
+import { useSelector } from "react-redux";
+import { createSelector } from "reselect";
+import { retrieveTopUsers } from "./selector";
+import { serverApi } from "../../../lib/data/config";
+import { Member } from "../../../lib/data/types/member";
 
-const activeUsers = [
-  { memberNick: "Martin", memberImage: "/img/martin.webp" },
-  { memberNick: "Justin", memberImage: "/img/justin.webp" },
-  { memberNick: "Rose", memberImage: "/img/rose.webp" },
-  { memberNick: "Nusret", memberImage: "/img/nusret.webp" },
-];
+/**  REDUX SLICE & SELECTOR */
+const topUsersRetriever = createSelector(retrieveTopUsers, (topUsers) => ({
+  topUsers,
+}));
 
 export default function ActiveUsers() {
+  const { topUsers } = useSelector(topUsersRetriever);
   return (
     <div className={"active-users-frame"}>
       <Container>
@@ -19,26 +23,29 @@ export default function ActiveUsers() {
           <Box className={"category-title"}>Active Users</Box>
           <Stack className={"cards-frame"}>
             <CssVarsProvider>
-              {activeUsers.length !== 0 ? (
-                activeUsers.map((member, index) => (
-                  <Card
-                    key={index}
-                    variant="outlined"
-                    sx={{ width: 300, borderRadius: "md", boxShadow: "sm" }}
-                  >
-                    <CardOverflow className="card-image">
-                      <AspectRatio ratio="1/1">
-                        <img src={member.memberImage} alt={member.memberNick} />
-                      </AspectRatio>
-                    </CardOverflow>
+              {topUsers.length !== 0 ? (
+                topUsers.map((member: Member) => {
+                  const imagePath = `${serverApi}/${member.memberImage}`;
+                  return (
+                    <Card
+                      key={member._id}
+                      variant="outlined"
+                      sx={{ width: 300, borderRadius: "md", boxShadow: "sm" }}
+                    >
+                      <CardOverflow className="card-image">
+                        <AspectRatio ratio="1/1">
+                          <img src={imagePath} alt={member.memberNick} />
+                        </AspectRatio>
+                      </CardOverflow>
 
-                    <Box sx={{ p: 0.5, textAlign: "center" }}>
-                      <Typography level="title-md">
-                        {member.memberNick}
-                      </Typography>
-                    </Box>
-                  </Card>
-                ))
+                      <Box sx={{ p: 0.5, textAlign: "center" }}>
+                        <Typography level="title-md">
+                          {member.memberNick}
+                        </Typography>
+                      </Box>
+                    </Card>
+                  );
+                })
               ) : (
                 <Box className="no-data">No Active Users</Box>
               )}
--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -5,29 +5,58 @@ import NewDishes from "./NewDishes";
 import Advertisement from "./Advertisement";
 import ActiveUsers from "./ActiveUsers";
 import Events from "./Events";
-import "../../../css/home.css";
-
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { Dispatch } from "@reduxjs/toolkit";
-import { createSelector } from "reselect";
-import { setPopularDishes } from "./slice";
-import { retrievePopularDishes } from "./selector";
+import { setNewDishes, setPopularDishes, setTopUsers } from "./slice";
 import { Product } from "../../../lib/data/types/products";
+import ProductService from "../../services/ProductService";
+import { ProductCollection } from "../../../lib/data/enums/product.enum";
+import MemberService from "../../services/MemberService";
+import { Member } from "../../../lib/data/types/member";
+import "../../../css/home.css";
 
 /**  REDUX SLICE & SELECTOR */
 const actionDispatch = (dispatch: Dispatch) => ({
   setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)),
+  setNewDishes: (data: Product[]) => dispatch(setNewDishes(data)),
+  setTopUsers: (data: Member[]) => dispatch(setTopUsers(data)),
 });
-const popularDishesRetriever = createSelector(
-  retrievePopularDishes,
-  (popularDishes) => ({ popularDishes })
-);
 
 export default function HomePage() {
-  const { setPopularDishes } = actionDispatch(useDispatch());
-  const { popularDishes } = useSelector(popularDishesRetriever);
+  const { setPopularDishes, setNewDishes, setTopUsers } = actionDispatch(
+    useDispatch()
+  );
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    const product = new ProductService();
+    product
+      .getProducts({
+        page: 1,
+        limit: 4,
+        order: "productViews",
+        productCollection: ProductCollection.DISH,
+      })
+      .then((data) => {
+        setPopularDishes(data);
+      })
+      .catch((err) => console.log(err));
+
+    product
+      .getProducts({
+        page: 1,
+        limit: 4,
+        order: "createdAt",
+        productCollection: ProductCollection.DISH,
+      })
+      .then((data) => setNewDishes(data))
+      .catch((err) => console.log(err));
+
+    const member = new MemberService();
+    member
+      .getTopUsers()
+      .then((data) => setTopUsers(data))
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
     <div className={"homepage"}>
